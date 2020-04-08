b8bf6bad96bee03f115799406204dfc42626b4d1
dkolyshnitsyn <dmitri.kolyshnitsyn@gmail.com>
2020-04-08 16:56:26 +0300

    adding nav menu & refactoring style css

--- src/index.js
+++ src/index.js
@@ -28,6 +28,24 @@ $('.slider_gal_wr').slick({
     slidesToScroll: 1,
 });
 
+$(".menu-icon").click(function(e){
+    e.preventDefault();
+    // $this = $(this);
+    if($(this).hasClass("is-opened")){
+        $(this).addClass("is-closed").removeClass("is-opened");
+    }else{
+        $(this).removeClass("is-closed").addClass("is-opened");
+    }
+});
+$(".menu-icon-cross").click(function(){
+    if ($(".top_mnu").is(":visible")){
+        $(".top_mnu").fadeOut(600);
+        $(".top_mnu li a").removeClass("fadeInUp animated");
+    } else {
+        $(".top_mnu").fadeIn(600);
+        $(".top_mnu li a").addClass("fadeInUp animated");
+    };
+});
 /// Gogle Maps 
 function initMap() {
 	let map = new google.maps.Map(document.getElementById('map'), {
